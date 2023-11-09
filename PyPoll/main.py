05ff4a147176da62da5ad17b345e0ebdc69448dc
John Olton <132177789+GreyDragon55@users.noreply.github.com>
2023-11-09 13:24:23 -0600

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,47 +1,76 @@
-import pandas as pd 
+import os
+import csv
 
-filepath= "./PyPoll/Resources/election_data.csv"
-df = pd.read_csv(filepath,sep = ",")
+election_data = os.path.join("election_data.csv")
 
-total_votes = df['Ballot ID'].count()
-candidates = df['Candidate'].unique()
+# A list to capture the names of candidates
+candidates = []
 
-number_of_votes_per_candidate=df.groupby('Candidate').agg({'Ballot ID' : 'count'})
-percentage_per_candidate = (number_of_votes_per_candidate['Ballot ID'] / total_votes) * 100
-percentage_per_candidate = percentage_per_candidate.sort_values(ascending=False)
-winner = percentage_per_candidate.index[0]
+# A list to capture the number of votes each candidate receives
+num_votes = []
 
+# A list to capture the percentage of total votes each candidate garners 
+percent_votes = []
 
-# Create a new DataFrame with the results
-results_df = pd.DataFrame({
-    'Candidate': percentage_per_candidate.index,
-    'Percentage of Votes': percentage_per_candidate.values,
-    'Total Votes': number_of_votes_per_candidate['Ballot ID'].values
-})
+# A counter for the total number of votes 
+total_votes = 0
 
-# Sort the results by percentage of votes in descending order
-results_df = results_df.sort_values('Candidate', ascending=True)
+with open(election_data, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    csv_header = next(csvreader)
 
-# Add a column with the formatted percentage of votes
-results_df['Percentage of Votes'] = results_df['Percentage of Votes'].map('{:.3f}%'.format)
+    for row in csvreader:
+        # Add to our vote-counter 
+        total_votes += 1 
 
-# Add a header with the total number of votes
-header = f'''
-Election Results
--------------------------
-Total Votes: {total_votes}
--------------------------
-'''
+        '''
+        If the candidate is not on our list, add his/her name to our list, along with 
+        a vote in his/her name.
+        If he/she is already on our list, we will simply add a vote in his/her
+        name 
+        '''
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            index = candidates.index(row[2])
+            num_votes.append(1)
+        else:
+            index = candidates.index(row[2])
+            num_votes[index] += 1
+    
+    # Add to percent_votes list 
+    for votes in num_votes:
+        percentage = (votes/total_votes) * 100
+        percentage = round(percentage)
+        percentage = "%.3f%%" % percentage
+        percent_votes.append(percentage)
+    
+    # Find the winning candidate
+    winner = max(num_votes)
+    index = num_votes.index(winner)
+    winning_candidate = candidates[index]
 
-# Add a footer with the winner
-footer = f'''
--------------------------
-Winner: {winner}
--------------------------
-'''
+# Displaying results
+print("Election Results")
+print("--------------------------")
+print(f"Total Votes: {str(total_votes)}")
+print("--------------------------")
+for i in range(len(candidates)):
+    print(f"{candidates[i]}: {str(percent_votes[i])} ({str(num_votes[i])})")
+print("--------------------------")
+print(f"Winner: {winning_candidate}")
+print("--------------------------")
 
-# Write the results to a CSV file
-with open('./PyPoll/analysis/election_results.csv', 'w') as f:
-    f.write(header)
-    results_df.to_csv(f, index=False)
-    f.write(footer)
+# Exporting to .txt file
+output = open("output.txt", "w")
+line1 = "Election Results"
+line2 = "--------------------------"
+line3 = str(f"Total Votes: {str(total_votes)}")
+line4 = str("--------------------------")
+output.write('{}\n{}\n{}\n{}\n'.format(line1, line2, line3, line4))
+for i in range(len(candidates)):
+    line = str(f"{candidates[i]}: {str(percent_votes[i])} ({str(num_votes[i])})")
+    output.write('{}\n'.format(line))
+line5 = "--------------------------"
+line6 = str(f"Winner: {winning_candidate}")
+line7 = "--------------------------"
+output.write('{}\n{}\n{}\n'.format(line5, line6, line7))
